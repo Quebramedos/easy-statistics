51f6fa77306f97faa2374dbfec1da0d69b65389b
Vinicius <viniciusvjo@outlook.com>
2018-10-31 23:50:22 -0300

    Adicionando os scripts

--- js/materialize.js
+++ js/materialize.js
@@ -7021,7 +7021,7 @@ $jscomp.polyfill = function (e, r, p, m) {
   };
 
   $(document).ready(function () {
-
+      
     document.addEventListener('DOMContentLoaded', function() {
       var elems = document.querySelectorAll('.dropdown-trigger');
       var instances = M.Dropdown.init(elems, options);
---
+++ script/validacaoDaTabela.js
@@ -0,0 +1,37 @@
+let vetorLimpo = []
+let vet100 = []
+function limpa(vet) {
+    for (let i = 0; i < vet.length - 1; ++i) {
+        vetorLimpo = vet.filter(function (este, i) {
+            return vet.indexOf(este) == i;
+        })
+    }
+    return vetorLimpo
+}
+
+function teste() {
+    let aux = vetorLimpo.length;
+    if (aux <= 7) {
+        console.log("Tabela Discreta")
+        total(vet100, vetorLimpo)
+    } else {
+        console.log("Tabela Contínua")
+        f_continua(vet100)
+    }
+}
+
+
+
+
+/*
+<body>
+    <br>
+    <label for="">Digite os Dados:</label>
+    <input type="text" id="Input">
+    <button id='bot' onclick="botao(),limpa(vet100), teste()">Enviar</button>
+    <div id="outTabela"></div>
+    <br>
+    <div id='outResul'></div>
+    <hr>
+</body>
+*/
