e982c538d49c18dd459cb42a210b37221e286f17
viniciusvjo <40768113+viniciusvjo@users.noreply.github.com>
2018-11-10 11:23:44 -0200

    input

--- script/importCorrelacao.js
+++ script/importCorrelacao.js
@@ -1,4 +1,4 @@
-let vet100 = []
+let vet100x = []
 let vet200
 let meioVetor
 var leitorDeCSV = new FileReader()
@@ -18,17 +18,17 @@ function leCSV(evt) {
     for (var i = 0; i < fileArr.length; i++) {
         fileArr[i] = fileArr[i].replace(/,/g, ".")
         //limpa a string e  joga no vetor
-        vet100[i] = fileArr[i].split(';').map(Number);
+        vet100x[i] = fileArr[i].split(';').map(Number);
     }
-    meioVetor = vet100.length / 2
+    meioVetor = vet100x.length / 2
     // console.log(vet100)
-    vet200 = vet100.slice(meioVetor, vet100.length)
-    console.log(vet100)
+    vet200 = vet100x.slice(meioVetor, vet100x.length)
+    console.log(vet100x)
     window.sessionStorage.setItem("vet200", vet200);
-    window.sessionStorage.setItem("vet100", vet100);
-    return vet100 // retorna o vetor 
+    window.sessionStorage.setItem("vet100x", vet100);
+    return vet100x // retorna o vetor 
 }
-console.log(vet100)
+
 /*
   <input type="file" id="inputCSV" onchange="pegaCSV(this)">
 */
