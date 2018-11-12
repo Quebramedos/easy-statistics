198c2b1a1e79d38e93a39cb2bc6f707b4eae74e3
viniciusvjo <40768113+viniciusvjo@users.noreply.github.com>
2018-11-12 02:08:03 -0200

    Update importCorrelacao.js

--- script/importCorrelacao.js
+++ script/importCorrelacao.js
@@ -1,10 +1,11 @@
 let vet100 = []
-let vet100x=[]
-let vet200
-let meioVetor
+let vet100x = []
+let vet200 = []
+let meioVetor = 0
 var leitorDeCSV = new FileReader()
+
 window.onload = function init() {
-    leitorDeCSV.onload = leCSV;
+    leitorDeCSV.onload = leCSVcorrelacao;
 }
 
 function pegaCSV(inputFile) {
@@ -12,23 +13,24 @@ function pegaCSV(inputFile) {
     leitorDeCSV.readAsText(file);
 }
 
-function leCSV(evt) {
+function leCSVcorrelacao(evt) {
     //recebe o arquivo e joga em um vetor
     var fileArr = evt.target.result.split('\n');
 
     for (var i = 0; i < fileArr.length; i++) {
         fileArr[i] = fileArr[i].replace(/,/g, ".")
         //limpa a string e  joga no vetor
-        vet100[i] = fileArr[i].split(';').map(Number);
+        vet100[i] = (fileArr[i].split(';')).map(Number)
+        alert(vet100)
     }
     meioVetor = vet100.length / 2
     // console.log(vet100)
-    vet100x = vet100.slice(0, meioVetor)
-    vet200 = vet100.slice(meioVetor, vet100.length)
-    console.log(vet100)
+    vet100x = (vet100.slice(0, meioVetor))
+    vet200 = vet100.slice(meioVetor, vet100.length - 1)
+
     window.sessionStorage.setItem("vet200", vet200);
     window.sessionStorage.setItem("vet100x", vet100x);
-    return vet100 // retorna o vetor 
+    return vet100x // retorna o vetor 
 }
 
 /*
